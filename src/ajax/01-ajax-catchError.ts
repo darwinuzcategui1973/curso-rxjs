55afade4184718d32c6c4cacc6059207a392ade3
Darwin Felipe Uzcategui Portatil Lenovo <darwin.uzcategui1973@hotmail.com>
2019-11-19 13:40:28 -0400

    Operadores Ajax Rxjs

---
+++ src/ajax/01-ajax-catchError.ts
@@ -0,0 +1,40 @@
+import { ajax, AjaxError } from "rxjs/ajax";
+import { map, pluck, catchError } from "rxjs/operators";
+import { of } from "rxjs";
+
+const url = "https://api.github.com/users?per_page=5";
+
+const manejaErrores = (respuesta: Response) => {
+  if (!respuesta.ok) {
+    throw new Error(respuesta.statusText);
+  }
+  return respuesta;
+};
+
+const atrapaError = (error: AjaxError) => {
+    console.warn("error en:", error.message);
+    return of([]);
+  };
+
+const fetchPromesa = fetch(url);
+
+// fetchPromesa
+//   .then(resp => resp.json())
+//   // .then(console.log)
+//   .then(data => console.log("data:", data))
+//   .catch(error => console.warn("error en usuario:", error));
+
+//   fetchPromesa
+//   .then(manejaErrores)
+//   .then(resp => resp.json())
+//   // .then(console.log)
+//   .then(data => console.log("data:", data))
+//   .catch(error => console.warn("error en usuario:", error));
+
+ajax(url)
+  .pipe(
+    // map(resp=> resp.response)
+    pluck("response"),
+    catchError(atrapaError)
+  )
+  .subscribe(users => console.log("usuarios", users));
