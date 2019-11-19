93c5cda48cffdd716d01094b27710ef3e9032304
Darwin Felipe Uzcategui Portatil Lenovo <darwin.uzcategui1973@hotmail.com>
2019-11-19 10:37:04 -0400

    operador thottleTime

---
+++ src/operadores-Tiempo/02-throttleTime.ts
@@ -0,0 +1,24 @@
+import { fromEvent, asyncScheduler } from "rxjs";
+import { throttleTime, pluck, distinctUntilChanged } from "rxjs/operators";
+
+const click$ = fromEvent(document, "click");
+
+click$.pipe(throttleTime(3000)).subscribe(console.log);
+
+// ejemplo 2
+
+const input = document.createElement("input");
+document.querySelector("body").append(input);
+
+const input$ = fromEvent(input, "keyup");
+
+input$
+  .pipe(
+    throttleTime(400, asyncScheduler, {
+      leading: false,
+      trailing: true
+    }),
+    distinctUntilChanged(),
+    pluck("target", "value")
+  )
+  .subscribe(console.log);
