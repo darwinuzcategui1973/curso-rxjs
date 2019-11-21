bbe190834f46d1b0fb3db8304817f062381f7e99
Darwin Felipe Uzcategui Gonzalez <Darwin.Uzcategui1973@hotmail.com>
2019-11-20 20:59:07 -0400

    operadores de conbinacion funcion concat

---
+++ src/combinaciones/03-funcion-concat.ts
@@ -0,0 +1,14 @@
+import { interval, concat, of } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+
+const interval$ = interval(1000);
+
+concat(
+    interval$.pipe( take(3) ),
+    interval$.pipe( take(2) ),
+    of(10,20)
+).subscribe( console.log )
+
+
+
